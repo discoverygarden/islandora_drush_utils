2b2a64a04627846abf95165f483885a176d11028
Akanksha Singh <akanksha@Akankshas-MacBook-Pro-2.local>
2022-10-13 20:21:00 +0530

    DGI9_133: Add user wrapper for rederive

--- src/Commands/Rederive.php
+++ src/Commands/Rederive.php
@@ -62,6 +62,8 @@ class Rederive extends DrushCommands {
    *
    * @command islandora_drush_utils:rederive
    * @aliases islandora_drush_utils:r,idu:r
+   *
+   * @dgi-i8-helper-user-wrap
    */
   public function rederive(array $options = [
     'source_uri' => 'http://pcdm.org/use#OriginalFile',
